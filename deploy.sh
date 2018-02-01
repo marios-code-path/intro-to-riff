943c6249a5a3b98267296650a1f4ff27d089dea3
Mario Gray <mario.gray@gmail.com>
2018-01-31 16:53:46 -0800

    Update scripts post Refactor

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,73 @@ DEMO_VERSION=0.0.1
 
 export DEMO_VERSION
 
-source ../bouncer.sh
-source ../dockerfn.sh
+function exitfn {
+	MSG=$1; shift
+	echo >&2 ${MSG}
+	exit 1
+}
+
+function exists_or_bail {
+	COMMAND=$1; shift
+	if type $COMMAND > /dev/null 2>&1
+	then : OK
+	else 
+		exitfn "${COMMAND} is required."
+	fi
+}
+
+function exists_or {
+	COMMAND=$1; shift
+	if type $COMMAND > /dev/null 2>&1
+	then : OK
+	else 
+		echo >&2 "${COMMAND} is required."	
+		return 1
+	fi
+	return 0
+}
+
+[[ -z ${RIFF_HOME} || ${RIFF_HOME} == .* ]] && exitfn "Set env \$RIFF_HOME to (absolute) root of local RIFF clone."
+
+exists_or_bail minikube
+exists_or_bail docker
+
+PARENT=${USERNAME}
+DELIM=""
+
+function docker_detect {
+    [[ -z ${DOCKER_HOST//} ]] && eval $(minikube docker-env)
+}
+
+# Single or Double namespace environment is assumed
+[[ -z ${PARENT//} ]] && DELIM="/"
+
+function docker_images {
+    set -v
+    docker_detect
+
+    echo docker images --filter=reference="${PARENT}${DELIM}'*'"
+}
+
+# input image container name 
+function docker_image_by_name {
+
+    IMAGE_NAME=$1; shift
+
+    docker_detect
+
+    docker images --filter=reference=${PARENT}${DELIM}${IMAGE_NAME} --format "{{.ID}}"
+}
+
+# input image container name
+function docker_rmi_by_name {
+
+    IMAGE_NAME=$1; shift
+
+    docker_detect
+
+    docker rmi `docker_image_by_name ${IMAGE_NAME}`
+}
 
 set -a -e -v
 
@@ -24,14 +89,14 @@ DEMO_JAR=target/demofn-${DEMO_VERSION}.jar
 
 riff create --name democonsumer --input reverse-out\
     --protocol pipes --artifact ${DEMO_JAR}\
-    --handler com.codepub.demo.DemoConsumer
+    --handler mcp.DemoConsumer
 
 riff create --name demofn --input reverse-in\
     --output reverse-out\
     --protocol pipes --artifact ${DEMO_JAR}\
-    --handler com.codepub.demo.DemoFunction
+    --handler mcp.DemoFunction
 
 riff create --name demosupplier --input supplier\
     --output reverse-in --protocol pipes\
     --artifact ${DEMO_JAR}\
-    --handler com.codepub.demo.DemoSupplier
+    --handler mcp.DemoSupplier
